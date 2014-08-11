a61bef21177ee35fd9aeb664eb24ec69f2e96b12
yousheng0222 <yousheng0222@gmail.com>
2014-08-11 16:14:13 +0800

    test

--- config/class/lesson.class.php
+++ config/class/lesson.class.php
@@ -84,7 +84,7 @@
 					$this->query($sql, $params);
 					if ($this->rowCount() != 1) {
 						$result = array(
-							'error' => 'There is something wrong when updating the data.'
+							'error' => 'There is something wrong when updating the data.1'
 						);
 					} else {
 						$result = array(
@@ -141,7 +141,7 @@
 					$this->query($sql, $params);
 					if ($this->rowCount() != 1) {
 						$result = array(
-							'error' => 'There is something wrong when updating the data.'
+							'error' => 'There is something wrong when updating the data.2'
 						);
 					} else {
 						$result = array(

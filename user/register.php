9e826b528aac045a086e958aa6b0c1e4e5c214d5
yousheng0222 <yousheng0222@gmail.com>
2014-08-13 14:10:28 +0800

    update password hash function
    update password hash function

--- user/register.php
+++ user/register.php
@@ -81,7 +81,7 @@ EOD;
 		</div>
 <?php display_footer(); ?>
 		<script>
-			$(document).ready(function(){$("#login").submit(function(){$("#submit").attr("disabled",true);$("#password").val(new jsSHA($("#password").val(),"TEXT").getHash("SHA-512","HEX",2048));$("#passward_check").val(new jsSHA($("#passward_check").val(),"TEXT").getHash("SHA-512","HEX",2048));$("#second_passward").val(new jsSHA($("#second_passward").val(),"TEXT").getHash("SHA-512","HEX",2048));});});
+			$(document).ready(function(){$("#register").submit(function(){$("#submit").attr("disabled",true);$("#password").val(new jsSHA($("#password").val(),"TEXT").getHash("SHA-512","HEX",2048));$("#passward_check").val(new jsSHA($("#passward_check").val(),"TEXT").getHash("SHA-512","HEX",2048));$("#second_passward").val(new jsSHA($("#second_passward").val(),"TEXT").getHash("SHA-512","HEX",2048));});});
 		</script>
 	</body>
 </html>

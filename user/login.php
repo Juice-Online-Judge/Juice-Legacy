15e9305a78a4f15dd6663ba6f64c9e190edb87f2
yousheng0222 <yousheng0222@gmail.com>
2014-08-13 23:38:52 +0800

    update

--- user/login.php
+++ user/login.php
@@ -38,42 +38,47 @@
 	<body>
 <?php display_navigation($prefix); ?>
 		<div id="main">
-			<div>
-				<div class="title center">
-					<h2>帳號登入</h2>
+			<div class="pure-g">
+				<div class="pure-u-2-3">
+					<p>Introduction</p>
 				</div>
+				<div class="pure-u-1-3">
+					<div class="title center">
+						<h2>帳號登入</h2>
+					</div>
 <?php
 	if (isset($message)) {
 		echo <<<EOD
-				<div class="warning center">
-						<h3>$message</h3>
-				</div>\n
+					<div class="warning center">
+							<h3>$message</h3>
+					</div>\n
 EOD;
 	}
 ?>
-				<div class="login-area">
-					<form name="login" id="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="pure-form pure-form-aligned">
-						<fieldset>
-							<div class="pure-control-group">
-								<label for="username">帳號：</label>
-								<input type="text" name="username" id="username" autocomplete="off" required>
-							</div>
-							<div class="pure-control-group">
-								<label for="passward">密碼：</label>
-								<input type="password" name="passward" id="password" autocomplete="off" required>
-							</div>
-							<div class="pure-control-group">
-								<label for="remember">記住我</label>
-								<input type="checkbox" name="remember" id="remember" value="1">
-							</div>
-							<div class="pure-control-group">
-								<input type="text" name="verify_code" id="verify_code" value="<?php echo (isset($verify_code)) ? $verify_code : $_COOKIE['verify_code_login']; ?>" hidden readonly autocomplete="off" required>
-							</div>
-							<div class="pure-controls">
-								<button type="submit" id="submit" class="pure-button pure-button-primary">登入</button>
-							</div>
-						</fieldset>	
-					</form>
+					<div class="login-area">
+						<form name="login" id="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="pure-form pure-form-aligned">
+							<fieldset>
+								<div class="pure-control-group">
+									<label for="username">帳號：</label>
+									<input type="text" name="username" id="username" autocomplete="off" required>
+								</div>
+								<div class="pure-control-group">
+									<label for="passward">密碼：</label>
+									<input type="password" name="passward" id="password" autocomplete="off" required>
+								</div>
+								<div class="pure-control-group">
+									<label for="remember">記住我</label>
+									<input type="checkbox" name="remember" id="remember" value="1">
+								</div>
+								<div class="pure-control-group">
+									<input type="text" name="verify_code" id="verify_code" value="<?php echo (isset($verify_code)) ? $verify_code : $_COOKIE['verify_code_login']; ?>" hidden readonly autocomplete="off" required>
+								</div>
+								<div class="pure-controls">
+									<button type="submit" id="submit" class="pure-button pure-button-primary">登入</button>
+								</div>
+							</fieldset>	
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>

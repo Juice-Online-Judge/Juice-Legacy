fa4dd3e13469b1798ff98a435e8564e05a81809f
yousheng0222 <yousheng0222@gmail.com>
2014-08-14 12:11:56 +0800

    update

--- config/web_view.php
+++ config/web_view.php
@@ -41,103 +41,107 @@ EOD;
 	function display_navigation($prefix) {
 ?>
 		<header>
-			<a href="<?php echo $prefix.'index.php' ?>"><div class="juice-icon"></div></a>
-			<nav id="demo-horizontal-menu">
-				<ul id="std-menu-items" class="juice-menu-head">
+			<div class="juice-logo">
+				<a href="<?php echo $prefix.'index.php' ?>"></a>
+			</div>
+			</div class="juice-menu">
+				<nav id="demo-horizontal-menu">
+					<ul id="std-menu-items">
 <?php
 		if (isset($_SESSION['uid'])) {
 ?>
-					<li><a href="<?php echo $prefix.'index.php' ?>">首頁</a></li>
-					<li>
-						<a href="#">主選單</a>
-						<ul>
-							<li><a href="#">課程教學</a></li>
-							<li class="pure-menu-separator"></li>
-							<li>
-								<a href="#">闖關競賽</a>
-								<ul>
-									<li><a href="#">排 行 榜</a></li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a href="#">會員中心</a>
-						<ul>
-						</ul>
-					</li>
+						<li><a href="<?php echo $prefix.'index.php' ?>">首頁</a></li>
+						<li>
+							<a href="#">主選單</a>
+							<ul>
+								<li><a href="#">課程教學</a></li>
+								<li class="pure-menu-separator"></li>
+								<li>
+									<a href="#">闖關競賽</a>
+									<ul>
+										<li><a href="#">排 行 榜</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">會員中心</a>
+							<ul>
+							</ul>
+						</li>
 <?php
 			if (true/*$_SESSION['admin_group'] > 0*/) {
 ?>
-					<li>
-						<a href="#">Juice</a>
-						<ul>
+						<li>
+							<a href="#">Juice</a>
+							<ul>
 <?php
 				if (true/*$_SESSION['admin_group'] > 3*/) {
 ?>
-							<li>
-								<a href="#">課程</a>
-								<ul>
-									<li><a href="<?php echo $prefix.'juice/lesson/lesson_list.php'; ?>">課程列表</a></li>
-									<li class="pure-menu-separator"></li>
-									<li><a href="<?php echo $prefix.'juice/lesson/lesson_refine.php'; ?>">新增課程</a></li>
-								</ul>
-							</li>
+								<li>
+									<a href="#">課程</a>
+									<ul>
+										<li><a href="<?php echo $prefix.'juice/lesson/lesson_list.php'; ?>">課程列表</a></li>
+										<li class="pure-menu-separator"></li>
+										<li><a href="<?php echo $prefix.'juice/lesson/lesson_refine.php'; ?>">新增課程</a></li>
+									</ul>
+								</li>
 <?php
 				}
 				if (true/*$_SESSION['admin_group'] > 6*/) {
 ?>
-							<li class="pure-menu-separator"></li>
-							<li>
-								<a href="#">會員</a>
-								<ul>
-									<li><a href="<?php echo $prefix.'juice/account/account_list.php'; ?>">帳號列表</a></li>
-									<li class="pure-menu-separator"></li>
-									<li><a href="<?php echo $prefix.'juice/account/account_modify.php'; ?>">帳號管理</a></li>
-								</ul>
-							</li>
+								<li class="pure-menu-separator"></li>
+								<li>
+									<a href="#">會員</a>
+									<ul>
+										<li><a href="<?php echo $prefix.'juice/account/account_list.php'; ?>">帳號列表</a></li>
+										<li class="pure-menu-separator"></li>
+										<li><a href="<?php echo $prefix.'juice/account/account_modify.php'; ?>">帳號管理</a></li>
+									</ul>
+								</li>
 <?php
 				}
 				if (true/*$_SESSION['admin_group'] > 9*/) {
 ?>
-							<li class="pure-menu-separator"></li>
-							<li>
-								<a href="#">網站</a>
-								<ul>
-									<li><a href="<?php echo $prefix.'juice/web/announcement_add.php'; ?>">新增公告</a></li>
-									<li class="pure-menu-separator"></li>
-									<li><a href="<?php echo $prefix.'juice/web/announcement_modify.php'; ?>">公告管理</a></li>
-									<li class="pure-menu-separator"></li>
-									<li><a href="<?php echo $prefix.'juice/web/web.php'; ?>">網站管理</a></li>
-								</ul>
-							</li>
+								<li class="pure-menu-separator"></li>
+								<li>
+									<a href="#">網站</a>
+									<ul>
+										<li><a href="<?php echo $prefix.'juice/web/announcement_add.php'; ?>">新增公告</a></li>
+										<li class="pure-menu-separator"></li>
+										<li><a href="<?php echo $prefix.'juice/web/announcement_modify.php'; ?>">公告管理</a></li>
+										<li class="pure-menu-separator"></li>
+										<li><a href="<?php echo $prefix.'juice/web/web.php'; ?>">網站管理</a></li>
+									</ul>
+								</li>
 <?php
 				}
 ?>
-						</ul>
-					</li>
+							</ul>
+						</li>
 <?php
 			}
 ?>
-					<li><a href="<?php echo $prefix.'user/logout.php'; ?>">登出</a></li>
+						<li><a href="<?php echo $prefix.'user/logout.php'; ?>">登出</a></li>
 <?php
 		} else {
 ?>
-					<li><a href="<?php echo $prefix.'user/login.php' ?>">登入</a></li>
-					<li><a href="<?php echo $prefix.'user/register.php' ?>">註冊</a></li>
+						<li><a href="<?php echo $prefix.'user/login.php' ?>">登入</a></li>
+						<li><a href="<?php echo $prefix.'user/register.php' ?>">註冊</a></li>
 <?php
 		}
 ?>
-					<li>
-						<a href="#">關於本站</a>
-						<ul>
-							<li><a href="#">團隊介紹</a></li>
-							<li class="pure-menu-separator"></li>
-							<li><a href="#">意見反饋</a></li>
-						</ul>
-					</li>
-				</ul>
-			</nav>
+						<li>
+							<a href="#">關於本站</a>
+							<ul>
+								<li><a href="#">團隊介紹</a></li>
+								<li class="pure-menu-separator"></li>
+								<li><a href="#">意見反饋</a></li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
 			<script>
 				YUI({classNamePrefix:"pure"}).use("gallery-sm-menu",function(a){var b=new a.Menu({container:"#demo-horizontal-menu",sourceNode:"#std-menu-items",orientation:"horizontal",hideOnOutsideClick:false,hideOnClick:false});b.render();b.show();});
 			</script>

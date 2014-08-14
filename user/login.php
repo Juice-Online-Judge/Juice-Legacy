b47a0609ce57155db0e440a70486c8bf68d5a600
IrisEyi <sdses9001@gmail.com>
2014-08-14 18:47:19 +0800

    Test

--- user/login.php
+++ user/login.php
@@ -38,7 +38,7 @@
 	<body>
 <?php display_navigation($prefix); ?>
 		<div id="main" class="pure-g">
-			<div class="pure-u-2-3" style="vertical-align: top !important;">
+			<div class="pure-u-2-3" style="vertical-align: middle !important;">
 				<p>Introduction</p>
 			</div>
 			<div class="pure-u-1-3">

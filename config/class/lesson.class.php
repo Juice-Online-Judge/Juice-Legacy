e038ec05df158e9b39ffb1fe9d9d9003b5d714e9
yousheng0222 <yousheng0222@gmail.com>
2014-08-17 19:50:17 +0800

    Fixed

--- config/class/lesson.class.php
+++ config/class/lesson.class.php
@@ -40,6 +40,7 @@
 					':practice_is_delete' => false
 				);
 				$this->query($sql, $params);
+				
 				array_push($result['practice'], $this->fetchAll());
 				$this->closeCursor();
 				$sql = "SELECT `implement_key`, `implement_content`, `time_limit`, `memory_limit`, `file_limit`, `mode`, `other_limit` FROM `lesson_implement` WHERE `lesson_id` = :lesson_id AND `implement_is_delete` = :implement_is_delete";

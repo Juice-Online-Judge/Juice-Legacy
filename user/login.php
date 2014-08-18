7188c41e114d788476d4b881e5fe97d4420bbf0c
IrisEyi <sdses9001@gmail.com>
2014-08-18 11:59:04 +0800

    Test

--- user/login.php
+++ user/login.php
@@ -42,7 +42,7 @@
 				<div class="pure-u-2-3 fullheight">
 					<p>Introduction</p>
 				</div>
-				<div class="pure-u-1-3 fullheight" style="align-self:stretch;">
+				<div class="pure-u-1-3 fullheight">
 					<div id="test2" class="shadow" style="position:relative; width:400px;">
 <?php
 	if (isset($message)) {

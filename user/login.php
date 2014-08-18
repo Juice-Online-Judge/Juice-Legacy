746837470356c68a27009f14dfe238f7b793ad51
IrisEyi <sdses9001@gmail.com>
2014-08-18 11:31:56 +0800

    Test

--- user/login.php
+++ user/login.php
@@ -82,7 +82,7 @@ EOD;
 		<script>
 			$(document).ready(function(){$("#login").submit(function(){$("#submit").attr("disabled",true);$("#password").val(new jsSHA($("#password").val(),"TEXT").getHash("SHA-512","HEX",2048));});});
 			$(document).ready(function(){
-				$('#test2').center({against:'parent'});
+				$('#test2').center({against:'#main'});
 			});
 		</script>
 	</body>

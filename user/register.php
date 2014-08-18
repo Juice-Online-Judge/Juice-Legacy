ad794dcc0a022513ee03e4a7b07305372e7b96d9
IrisEyi <sdses9001@gmail.com>
2014-08-18 12:17:23 +0800

    Test

--- user/register.php
+++ user/register.php
@@ -83,7 +83,7 @@ EOD;
 								<div style="display:hidden;">
 									<input type="text" name="verify_code" id="verify_code" value="<?php echo (isset($verify_code)) ? $verify_code : $_COOKIE['verify_code_register']; ?>" hidden readonly autocomplete="off" required>
 								</div>
-								<div class="pure-control-group">
+								<div class="pure-control-group center">
 									<button type="submit" id="submit" class="pure-button pure-button-primary">註冊</button>
 								</div>
 							</fieldset>

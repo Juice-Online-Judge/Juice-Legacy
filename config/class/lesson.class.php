7b9fdf0a0f5dba9e3502fc64a500b359b70f8d83
yousheng0222 <yousheng0222@gmail.com>
2014-08-18 22:35:49 +0800

    test

--- config/class/lesson.class.php
+++ config/class/lesson.class.php
@@ -224,7 +224,7 @@
 										$this->query($sql, $params);
 										if ($this->rowCount() != 1) {
 											$result = array(
-												'error' => 'There is something wrong when updating the data.'
+												'error' => 'There is something wrong when updating the data.'.$this->stmt_errorInfo()
 											);
 											$this->closeCursor();
 											break 2;

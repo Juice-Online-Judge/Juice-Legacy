b4029edac9230a3ed5b77dd990502ae590a33ded
IrisEyi <sdses9001@gmail.com>
2014-08-18 12:07:47 +0800

    Test

--- user/login.php
+++ user/login.php
@@ -70,8 +70,8 @@ EOD;
 								<div style="display:hidden;">
 									<input type="text" name="verify_code" id="verify_code" value="<?php echo (isset($verify_code)) ? $verify_code : $_COOKIE['verify_code_login']; ?>" hidden readonly autocomplete="off" required>
 								</div>
-								<div class="pure-control-group">
-									<button type="submit" id="submit" class="pure-button pure-button-primary center">登入</button>
+								<div class="pure-control-group center">
+									<button type="submit" id="submit" class="pure-button pure-button-primary">登入</button>
 								</div>
 							</fieldset>	
 						</form>
--- user/register.php
+++ user/register.php
@@ -84,7 +84,7 @@ EOD;
 									<input type="text" name="verify_code" id="verify_code" value="<?php echo (isset($verify_code)) ? $verify_code : $_COOKIE['verify_code_register']; ?>" hidden readonly autocomplete="off" required>
 								</div>
 								<div class="pure-control-group">
-									<button type="submit" id="submit" class="pure-button pure-button-primary center">註冊</button>
+									<button type="submit" id="submit" class="pure-button pure-button-primary">註冊</button>
 								</div>
 							</fieldset>
 						</form>

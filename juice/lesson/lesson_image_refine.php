b5ea30a4127b338f9a386bcfe36ed7df5fa16394
yousheng0222 <yousheng0222@gmail.com>
2014-08-21 01:19:31 +0800

    Fixed

--- juice/lesson/lesson_image_refine.php
+++ juice/lesson/lesson_image_refine.php
@@ -11,7 +11,7 @@
 		header("Location: ".$prefix."index.php");
 		exit();
 	}
-	
+	/*
 	if (isset($_POST['verify_code']) and isset($_COOKIE['verify_code_login']) and $_COOKIE['verify_code_login'] == $_POST['verify_code']) {
 		foreach ($_FILES['files']['name'] as $key => $name) {     
 			if ($_FILES['files']['error'][$key] != 0) {
@@ -30,7 +30,7 @@
 	} else {
 		$message = '登入頁面已失效，請重新登入';
 	}
-	
+	*/
 	$verify_code = verify_code();
 	setcookie("verify_code_upload_image", $verify_code, $current_time + 600, '/', '', false, true);
 ?>
@@ -51,7 +51,7 @@
 						<fieldset>
 							<div class="pure-control-group">
 								<label for="files">圖片：</label>
-								<input type="file" id="files" name="files[]" multiple="multiple" accept="image/*">
+								<input type="file" id="files" name="files[]" accept="image/*">
 							</div>
 							<div style="display:hidden;">
 								<input type="text" name="verify_code" id="verify_code" value="<?php echo (isset($verify_code)) ? $verify_code : $_COOKIE['verify_code_upload_image']; ?>" hidden readonly autocomplete="off" required>

48119279bbb5bed39eade4314f85a976ce61d5c8
yousheng0222 <yousheng0222@gmail.com>
2014-08-21 01:10:22 +0800

    Fixed

--- juice/lesson/lesson_image_refine.php
+++ juice/lesson/lesson_image_refine.php
@@ -12,20 +12,37 @@
 		exit();
 	}
 	
-	if (isset($_POST['username']) and isset($_POST['passward'])) {
-		if (isset($_POST['verify_code']) and isset($_COOKIE['verify_code_login']) and $_COOKIE['verify_code_login'] == $_POST['verify_code']) {
-			$remember = (isset($_POST['remember'])) ? 1 : 0;
-			$login = new account('mysql', DATABASE_MYSQL_HOST, DATABASE_MYSQL_DBNAME, DATABASE_MYSQL_USERNAME, DATABASE_MYSQL_PASSWORD);
-			$message = $login->login($_POST['username'], $_POST['passward'], $remember);
-			if ($message === true) {
-				setcookie("verify_code_login", '', $current_time - 600, '/', '', false, true);
-				header("Location: ".$prefix."index.php");
-				exit();
+	if (isset($_POST['verify_code']) and isset($_COOKIE['verify_code_login']) and $_COOKIE['verify_code_login'] == $_POST['verify_code']) {
+		foreach ($_FILES['files']['name'] as $f => $name) {     
+			if ($_FILES['files']['error'][$f] == 4) {
+				continue; // Skip file if any error found
+			}	       
+			if ($_FILES['files']['error'][$f] == 0) {	           
+				if ($_FILES['files']['size'][$f] > $max_file_size) {
+					$message[] = "$name is too large!.";
+					continue; // Skip large files
+				}
+				elseif( ! in_array(pathinfo($name, PATHINFO_EXTENSION), $valid_formats) ){
+					$message[] = "$name is not a valid format";
+					continue; // Skip invalid file formats
+				}
+				else{ // No error found! Move uploaded files 
+					if(move_uploaded_file($_FILES["files"]["tmp_name"][$f], $path.$name))
+					$count++; // Number of successfully uploaded file
+				}
 			}
-		} else {
-			$message = '登入頁面已失效，請重新登入';
 		}
+		$login = new account('mysql', DATABASE_MYSQL_HOST, DATABASE_MYSQL_DBNAME, DATABASE_MYSQL_USERNAME, DATABASE_MYSQL_PASSWORD);
+		$message = $login->login($_POST['username'], $_POST['passward'], $remember);
+		if ($message === true) {
+			setcookie("verify_code_login", '', $current_time - 600, '/', '', false, true);
+			header("Location: ".$prefix."index.php");
+			exit();
+		}
+	} else {
+		$message = '登入頁面已失效，請重新登入';
 	}
+	
 	$verify_code = verify_code();
 	setcookie("verify_code_upload_image", $verify_code, $current_time + 600, '/', '', false, true);
 ?>
@@ -45,8 +62,8 @@
 					<form name="upload_image" id="upload_image" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="pure-form pure-form-aligned">
 						<fieldset>
 							<div class="pure-control-group">
-								<label for="file">圖片：</label>
-								<input type="file" id="file" name="files[]" multiple="multiple" accept="image/*">
+								<label for="files">圖片：</label>
+								<input type="file" id="files" name="files[]" multiple="multiple" accept="image/*">
 							</div>
 							<div style="display:hidden;">
 								<input type="text" name="verify_code" id="verify_code" value="<?php echo (isset($verify_code)) ? $verify_code : $_COOKIE['verify_code_upload_image']; ?>" hidden readonly autocomplete="off" required>

465deff4f26ec3f6a065212d6594c6e1210e72a8
yousheng0222 <yousheng0222@gmail.com>
2014-08-21 10:24:34 +0800

    Update

---
+++ others/show_imgages.php
@@ -0,0 +1,13 @@
+<?php
+	if (!isset($prefix)) {
+		$prefix = '../';
+	}
+	require_once $prefix."config/web_preprocess.php";
+	
+	$image = new lesson('mysql', DATABASE_MYSQL_HOST, DATABASE_MYSQL_DBNAME, DATABASE_MYSQL_USERNAME, DATABASE_MYSQL_PASSWORD);
+	$result = $image->show_image($_GET['key'], $_GET['image_key']);
+	if (!isset($result['error'])) {
+		header('Content-type: '.$result['image_type']);
+		echo $result['image_data'];
+	}
+?>

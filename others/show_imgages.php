300a39941720e99efb96239d1e4e047e2e7ca05b
yousheng0222 <yousheng0222@gmail.com>
2014-08-21 11:04:52 +0800

    Fixed

--- others/show_imgages.php
+++ others/show_imgages.php
@@ -6,7 +6,7 @@
 	
 	$image = new lesson('mysql', DATABASE_MYSQL_HOST, DATABASE_MYSQL_DBNAME, DATABASE_MYSQL_USERNAME, DATABASE_MYSQL_PASSWORD);
 	$result = $image->show_image($_GET['key'], $_GET['image_key']);
-	if (!isset($result['error'])) {
+	if (!isset($result['error']) and isset($result['image_data'])) {
 		header('Content-type: '.$result['image_type']);
 		echo $result['image_data'];
 	}

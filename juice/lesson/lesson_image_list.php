b8b02ed9f29cd6e7319e1386d83df8341d7c7a4c
yousheng0222 <yousheng0222@gmail.com>
2014-08-21 10:51:05 +0800

    Update

--- juice/lesson/lesson_image_list.php
+++ juice/lesson/lesson_image_list.php
@@ -50,7 +50,7 @@
 ?>
 									<tr>
 										<td><?php echo $i++; ?></td>
-										<td><img src="<?php echo $prefix.'others/show_imgages.php?key='.$_GET['key'].'&image_key='.$tmp['image_key']; ?>" style="max-width:480px;"></td>
+										<td><a href="<?php echo $prefix.'others/show_imgages.php?key='.$_GET['key'].'&image_key='.$tmp['image_key']; ?>" target="_blank"><img src="<?php echo $prefix.'others/show_imgages.php?key='.$_GET['key'].'&image_key='.$tmp['image_key']; ?>" style="max-width:480px;"></a></td>
 										<td><input type="text" value="<?php echo 'http://'.WEB_DOMAIN_NAME.'/freedom/juice/others/show_imgages.php?key='.$_GET['key'].'&image_key='.$tmp['image_key']; ?>"></td>
 									</tr>
 <?php

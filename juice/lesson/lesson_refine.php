486c1d0d2479e08c9e73f5a69599a7eb94f694fe
IrisEyi <sdses9001@gmail.com>
2014-08-22 19:07:22 +0800

    Update

--- juice/lesson/lesson_refine.php
+++ juice/lesson/lesson_refine.php
@@ -69,7 +69,7 @@
 ?>
 								<div>
 									<div class="pure-control-group">
-										<label for="practice" style="text-align:left;width:auto;"><h3>填空練習：</h3></label>
+										<label for="practice" style="text-align:left;width:auto;"><h3>填空練習 第 <?php echo $i; ?> 題：</h3></label>
 										<textarea class="ckeditor" name="practice_id_<?php echo $i; ?>" id="practice_id_<?php echo $i; ?>" required></textarea>
 										<input type="text" name="practice_action_<?php echo $i; ?>" id="practice_action_<?php echo $i; ?>" value="add" hidden readonly autocomplete="off">
 									</div>
@@ -108,7 +108,7 @@
 										<input type="text" name="implement_otherLimit_<?php echo $i; ?>" id="implement_otherLimit_<?php echo $i; ?>" value="<?php echo $tmp['other_limit']; ?>" autocomplete="off" required>
 									</div>
 									<div class="pure-control-group pure-u-1-1">
-										<label for="implement" style="text-align:left;"><h3>動 動 腦 第 <?php echo $i; ?> 題：</h3></label>
+										<label for="implement" style="text-align:left;width:auto;"><h3>動動腦 第 <?php echo $i; ?> 題：</h3></label>
 										<textarea class="ckeditor" name="implement_id_<?php echo $i; ?>" id="implement_id_<?php echo $i; ?>" required><?php echo $tmp['implement_content']; ?></textarea>
 										<input type="text" name="implement_key_<?php echo $i; ?>" id="implement_key_<?php echo $i; ?>" value="<?php echo $tmp['implement_key']; ?>" hidden readonly autocomplete="off">
 										<input type="text" name="implement_action_<?php echo $i; ?>" id="implement_action_<?php echo $i; ?>" value="update" hidden readonly autocomplete="off">
@@ -144,7 +144,7 @@
 										<input type="text" name="implement_otherLimit_<?php echo $i; ?>" id="implement_otherLimit_<?php echo $i; ?>" value="" autocomplete="off" required>
 									</div>
 									<div class="pure-control-group pure-u-1-1">
-										<label for="implement" style="text-align:left;"><h3>動 動 腦 第 <?php echo $i; ?> 題：</h3></label>
+										<label for="implement" style="text-align:left;width:auto;"><h3>動動腦 第 <?php echo $i; ?> 題：</h3></label>
 										<textarea class="ckeditor" name="implement_id_<?php echo $i; ?>" id="implement_id_<?php echo $i; ?>" required></textarea>
 										<input type="text" name="implement_action_<?php echo $i; ?>" id="implement_action_<?php echo $i; ?>" value="add" hidden readonly autocomplete="off">
 									</div>

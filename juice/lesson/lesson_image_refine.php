a5902681e8a5c250af903945a1f5b0ef2b1ed487
yousheng0222 <yousheng0222@gmail.com>
2014-08-22 17:29:48 +0800

    update

--- juice/lesson/lesson_image_refine.php
+++ juice/lesson/lesson_image_refine.php
@@ -68,7 +68,6 @@ EOD;
 	for ($i = (count($result) - 1); $i >= 0; $i--) {
 ?>
 <<<<<<< HEAD
-										<option value="<?php echo $tmp['lesson_key']; ?>"><?php echo $tmp['lesson_unit']; ?></option>
 =======
 									<option value="<?php echo $result[$i]['lesson_key']; ?>"><?php echo $result[$i]['lesson_unit']; ?></option>
 >>>>>>> origin/master

e20091a32a06705efacad59e11ca5f26843812eb
Lyrisbee <xicadfta@gmail.com>
2014-08-23 14:16:54 +0800

    Test

--- about/team.php
+++ about/team.php
@@ -65,12 +65,12 @@
 				$("div[id*='introduction']").css('height',adjustheight).css('overflow','hidden');
 				$(".more-less").append('<p class="continue">...</p><a herf="#" class="adjust">more</a>');
 				$(".adjust").toggle(function(){
-					$(this).parent.children(".more-less").children('div[id*="introduction"]').css('height','auto').css('overflow','visible');
-					$(this).parent.find("p.continue").css('display','none');
+					$(this).parent().children(".more-less").children('div[id*="introduction"]').css('height','auto').css('overflow','visible');
+					$(this).parent().find("p.continue").css('display','none');
 					$(this).text("less");
 				} , function(){
-					$(this).parent.children(".more-less").children('div[id*="introduction"]').css('height',adjustheight).css('overflow','hidden');
-					$(this).parent.find("p.continue").css('display','block');
+					$(this).parent().children(".more-less").children('div[id*="introduction"]').css('height',adjustheight).css('overflow','hidden');
+					$(this).parent().find("p.continue").css('display','block');
 					$(this).text("more");
 				});
 			});
@@ -79,6 +79,7 @@
 				$('div[id*="introduction"]').hide();
 				$('div[id*="member"]').click(function(){
 					$(this).parent().children(".more-less").slideToggle();
+					$(this).parent().children(".more-less").children('div[id*="introduction"]').slideToggle();
 				});
 				
 			});

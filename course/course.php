6b75e4e2153d1d11b0df48a923bb2b86025b5659
yousheng0222 <yousheng0222@gmail.com>
2014-08-24 17:08:43 +0800

    Fuxed

--- course/course.php
+++ course/course.php
@@ -4,7 +4,7 @@
 	}
 	require_once $prefix.'config/web_preprocess.php';
 	
-	if (permission_check('login')) {
+	if (!permission_check('login')) {
 		header("Location: ".$prefix."index.php");
 		exit();
 	} else if (!isset($_GET['unit'])) {

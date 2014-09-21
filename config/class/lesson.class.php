37df09f8c13ac964ed6be8ebcc273951d575c6a2
Believedecision <Believedecision@users.noreply.github.com>
2014-09-21 12:37:40 +0800

    Test

--- config/class/lesson.class.php
+++ config/class/lesson.class.php
@@ -284,7 +284,7 @@
 									$this->query($sql, $params);
 									if ($this->rowCount() != 1) {
 										$result = array(
-											'error' => 'There is something wrong when updating the data.'
+											'error' => 'There is something wrong when updating the data.'.$params[':lesson_unit']
 										);
 										$this->closeCursor();
 										break 2;

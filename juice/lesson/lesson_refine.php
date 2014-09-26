56b7958ed647c3a28ef0eb5f1700b018b6492a68
Believedecision <Believedecision@users.noreply.github.com>
2014-09-26 11:33:13 +0800

    Test

--- juice/lesson/lesson_refine.php
+++ juice/lesson/lesson_refine.php
@@ -31,7 +31,7 @@
 		<!--<link rel="icon" href="" type="image/x-icon">-->
 <?php display_css_link($prefix); ?>
 <?php display_scripts_link(); ?>
-		<script src="http://cdn.ckeditor.com/4.4.5/full/ckeditor.js"></script>
+		<script src="http://cdn.ckeditor.com/4.4.5/full-all/ckeditor.js"></script>
 	</head>
 	<body>
 <?php display_navigation($prefix); ?>

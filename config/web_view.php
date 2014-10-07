e26254371c08d52b8e08bdbcbe7af35295530eda
IrisEyi <sdses9001@gmail.com>
2014-10-08 01:06:52 +0800

    Test

--- config/web_view.php
+++ config/web_view.php
@@ -115,9 +115,9 @@
 ?>
 		<footer>
 			<div>
-				<span><a href="<?php echo $prefix.'about/index.php' ?>">關於本站</a></span>
-				<span><a href="<?php echo $prefix.'about/team.php' ?>">團隊介紹</a></span>
-				<span><a href="#">意見回饋</a></span>
+				<span class="link"><a href="<?php echo $prefix.'about/index.php' ?>">>關於本站</a></span>
+				<span class="link"><a href="<?php echo $prefix.'about/team.php' ?>">>團隊介紹</a></span>
+				<span class="link"><a href="#">>意見回饋</a></span>
 			</div>
 			<div>
 				<span>Copyright © 2014 Juice All rights reserved.</span>

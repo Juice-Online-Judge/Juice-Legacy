f7f99a747cd099e1e2f96d7b6f709b644452f8ac
IrisEyi <sdses9001@gmail.com>
2014-10-08 00:36:24 +0800

    Fix

--- course/course_list.php
+++ course/course_list.php
@@ -36,11 +36,13 @@
 		$lesson_level_name = array('初階', '中階', '高階', '終階');
 		foreach ($result as $tmp) {
 ?>
-							<div id="course_option">
-								<p>單　　元　　<?php echo $tmp['lesson_unit']; ?></p>
-								<p><?php echo $lesson_level_name[$tmp['lesson_level']-1]; ?></p>
-								<a href="http://crux.coder.tw/freedom/juice/course/course.php?unit=<?php echo $tmp['lesson_unit']; ?>"><?php echo $tmp['lesson_title']; ?></a>
-							</div>
+							<a href="http://crux.coder.tw/freedom/juice/course/course.php?unit=<?php echo $tmp['lesson_unit']; ?>">
+								<div id="course_option">
+									<p>單　　元　　<?php echo $tmp['lesson_unit']; ?></p>
+									<p><?php echo $lesson_level_name[$tmp['lesson_level']-1]; ?></p>
+									<p><?php echo $tmp['lesson_title']; ?></p>
+								</div>
+							</a>
 <?php
 		}
 	}

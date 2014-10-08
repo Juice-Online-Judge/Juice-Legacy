7367b986457403905ffbcba702b5430bdec858e2
IrisEyi <sdses9001@gmail.com>
2014-10-08 17:18:13 +0800

    Test

--- user/login.php
+++ user/login.php
@@ -39,7 +39,7 @@
 	<body>
 <?php display_navigation($prefix); ?>
 		<div id="main">
-			<div class="flexblock">
+			<div class="flexblock fullheight">
 				<div class="pure-u-2-3">
 					<p>Introduction</p>
 				</div>

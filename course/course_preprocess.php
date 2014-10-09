4348ea0117723b5bd316ff976be2fc1920417083
Believedecision <Believedecision@users.noreply.github.com>
2014-10-09 14:09:51 +0800

    Update

--- course/course_preprocess.php
+++ course/course_preprocess.php
@@ -15,7 +15,7 @@
 				$judge = new judge('mysql', DATABASE_MYSQL_HOST, DATABASE_MYSQL_DBNAME, DATABASE_MYSQL_USERNAME, DATABASE_MYSQL_PASSWORD);
 				$result = $judge->code_submit('lesson_implement', $value, $_POST['implement_key']);
 				if (!isset($result['error'])) {
-					$command = 'ruby '.$prefix.'juice/core/lib/juice_judge.rb ';
+					$command = 'ruby2.0 '.$prefix.'juice/core/lib/juice_judge.rb ';
 					$command .= $result['key'].' '.$result['table'].' '.$_POST['implement_key'];
 					$outputFile = '/dev/null';
 					shell_exec(sprintf('%s > %s 2>&1 & echo $!', $command, $outputFile));
